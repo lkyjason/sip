365640b91439239a052a7df921df96e055d2b5e8
Jason Lee <meiyingl@andrew.cmu.edu>
2020-05-16 18:28:14 +0000

    server networking basics

--- server.c
+++ server.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define MAX_LISTEN_QUEUE 10
 
+void print_addr(struct addrinfo *p) {
+
+    char ipstr[INET6_ADDRSTRLEN];
+    char *ipver;
+    void *addr;
+
+    if (p->ai_family == AF_INET) { // IPv4
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+        addr = &(ipv4->sin_addr);
+        ipver = "IPv4";
+    } else { // IPv6
+        struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+        addr = &(ipv6->sin6_addr);
+        ipver = "IPv6";
+    }
+
+    inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+    printf("addr is %s %s\n", ipver, ipstr);
+}
 
 int setup_listen(char *port) {
     int listenfd, rc;
@@ -26,6 +47,9 @@ int setup_listen(char *port) {
     }
 
     for(ptr = servinfo ; ptr != NULL ; ptr = ptr->ai_next) {
+        // print info
+        print_addr(ptr);
+
         // get listen socket
         if( (listenfd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol) ) == -1 ) { 
             perror("[SERVER:SOCKET]");
@@ -43,11 +67,14 @@ int setup_listen(char *port) {
             perror("[SERVER:BIND]");
             continue;
         }
+
+        break;
     }
 
+    if(ptr == NULL) listenfd = -1;
+
     freeaddrinfo(servinfo);
-    if(ptr == NULL) return -1;
-    else return listenfd;
+    return listenfd;
 }
 
 
@@ -55,7 +82,7 @@ int main(int argc, char *argv[]) {
     int rc, listenfd;
 
     if(argc != 2) {
-        printf("./sip_server <PORT>");
+        printf("./sip_server <PORT>\n");
         exit(1);
     }
 
@@ -72,11 +99,25 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    printf("server listening on port %s", port);
+    printf("server listening on port %s\n", port);
+
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size = sizeof(their_addr);
+    int clientfd;
 
     // main accept loop
     while(1) {
+        if( (clientfd = accept(listenfd, (struct sockaddr *)&their_addr, &sin_size) ) == -1 ) {
+            perror("[SERVER:ACCEPT]");
+            continue;
+        }
 
+        printf("hello world...\n");
+        fflush(stdout);
+
+        // print_addr((struct sockaddr *) &their_addr);
+
+        close(clientfd);
     }
 
     return 0;
